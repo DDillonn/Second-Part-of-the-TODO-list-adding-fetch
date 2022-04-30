62250d7c026cd80d870b58d1c84b0c3ff0db6da7
DDillonn <Saltnessdillon@gmail.com>
2022-04-30 20:32:54 +0000

    Created Repo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,90 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+export function Home() {
+	const [todoList, setTodoList] = useState([]);
+	const [isShown, setIsShown] = useState({
+		state: false,
+		index: 0
+	});
 
-//create your first component
-const Home = () => {
+	const APIURL = "https://assets.breatheco.de/apis/fake/todos/user/DDillonn";
+
+	const getSampleTask = () => {
+		fetch(APIURL)
+			.then(resp => resp.json())
+			.then(newTodo => setTodoList(newTodo))
+			.then(resp => console.log(resp));
+	};
+
+	const updateAPI = () => {
+		fetch(APIURL, {
+			method: "PUT",
+			body: JSON.stringify([
+				{ label: "Make the bed", done: false },
+				{ label: "Walk the dog", done: false },
+				{ label: "Do the replits", done: false }
+			]),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(response => response.json())
+			.then(response => console.log(response));
+	};
+
+	useEffect(() => {
+		getSampleTask();
+		updateAPI();
+	}, []);
+
+	const todo = todoList.map((item, i) => {
+		return (
+			<div className="repeating" key={i}>
+				<li
+					onMouseEnter={() => setIsShown({ state: true, index: i })}
+					onMouseLeave={() => setIsShown({ state: false, index: 0 })}>
+					{item.label}
+
+					{isShown.state === true && isShown.index === i ? (
+						<button onClick={() => removeItem(i)}>X</button>
+					) : (
+						""
+					)}
+				</li>
+			</div>
+		);
+	});
+
+	const removeItem = index => {
+		const newArray = todoList.filter((item, i) => i != index);
+		setTodoList(newArray);
+	};
+
+	const newTodo = onKeyDownEvent => {
+		if (onKeyDownEvent.keyCode === 13) {
+			let userInput = onKeyDownEvent.target.value;
+			const newTodo = [...todoList, { label: userInput, done: false }];
+			setTodoList(newTodo);
+			onKeyDownEvent.target.value = [];
+		}
+	};
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="box">
+			<h1 className="text-center">todos</h1>
+			<input
+				onKeyDown={newTodo}
+				type="text"
+				id="fname"
+				placeholder="What needs to be done?"
+				name="fname"></input>
+			<div>
+				<ul>{todo}</ul>
+				<div>
+					<ul className="counter">{todo.length} item left</ul>
+				</div>
+			</div>
 		</div>
 	);
-};
-
-export default Home;
+}
